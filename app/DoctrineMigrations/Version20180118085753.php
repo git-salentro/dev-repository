96a8a0499f671550573bfd0dc359628ea8626822
Dmitry Jumagaliev <d.jumagaliev@ocsico.com>
2018-01-22 14:00:13 +0300

    Migration Fix

--- app/DoctrineMigrations/Version20180118085753.php
+++ app/DoctrineMigrations/Version20180118085753.php
@@ -19,9 +19,8 @@ class Version20180118085753 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
 
-        $this->addSql('ALTER TABLE stripe_account ADD city_id INT DEFAULT NULL, ADD line1 VARCHAR(255) DEFAULT NULL, ADD postal_code VARCHAR(255) DEFAULT NULL, ADD state VARCHAR(255) DEFAULT NULL, ADD business_name VARCHAR(255) DEFAULT NULL, ADD business_tax_id VARCHAR(255) DEFAULT NULL, ADD date_of_birth DATE DEFAULT NULL, ADD first_name VARCHAR(255) DEFAULT NULL, ADD last_name VARCHAR(255) DEFAULT NULL, ADD ssn_last4 VARCHAR(255) DEFAULT NULL, ADD type VARCHAR(255) DEFAULT NULL, ADD tos_acceptance_date DATE DEFAULT NULL, ADD tos_acceptance_ip VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE stripe_account ADD CONSTRAINT FK_52F1675E8BAC62AF FOREIGN KEY (city_id) REFERENCES cities (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_52F1675E8BAC62AF ON stripe_account (city_id)');
+        $this->addSql('ALTER TABLE stripe_account  ADD line1 VARCHAR(255) DEFAULT NULL, ADD postal_code VARCHAR(255) DEFAULT NULL, ADD state VARCHAR(255) DEFAULT NULL, ADD business_name VARCHAR(255) DEFAULT NULL, ADD business_tax_id VARCHAR(255) DEFAULT NULL, ADD date_of_birth DATE DEFAULT NULL, ADD first_name VARCHAR(255) DEFAULT NULL, ADD last_name VARCHAR(255) DEFAULT NULL, ADD ssn_last4 VARCHAR(255) DEFAULT NULL, ADD type VARCHAR(255) DEFAULT NULL, ADD tos_acceptance_date DATE DEFAULT NULL, ADD tos_acceptance_ip VARCHAR(255) DEFAULT NULL');
+
     }
 
     /**
@@ -34,6 +33,6 @@ class Version20180118085753 extends AbstractMigration
 
         $this->addSql('ALTER TABLE stripe_account DROP FOREIGN KEY FK_52F1675E8BAC62AF');
         $this->addSql('DROP INDEX UNIQ_52F1675E8BAC62AF ON stripe_account');
-        $this->addSql('ALTER TABLE stripe_account DROP city_id, DROP line1, DROP postal_code, DROP state, DROP business_name, DROP business_tax_id, DROP date_of_birth, DROP first_name, DROP last_name, DROP ssn_last4, DROP type, DROP tos_acceptance_date, DROP tos_acceptance_ip');
+        $this->addSql('ALTER TABLE stripe_account DROP line1, DROP postal_code, DROP state, DROP business_name, DROP business_tax_id, DROP date_of_birth, DROP first_name, DROP last_name, DROP ssn_last4, DROP type, DROP tos_acceptance_date, DROP tos_acceptance_ip');
     }
 }
--- app/DoctrineMigrations/Version20180119083554.php
+++ app/DoctrineMigrations/Version20180119083554.php
@@ -18,10 +18,7 @@ class Version20180119083554 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-
-        $this->addSql('ALTER TABLE stripe_account DROP FOREIGN KEY FK_52F1675E8BAC62AF');
-        $this->addSql('DROP INDEX UNIQ_52F1675E8BAC62AF ON stripe_account');
-        $this->addSql('ALTER TABLE stripe_account ADD city VARCHAR(255) DEFAULT NOT NULL, ADD day_of_birth VARCHAR(255) DEFAULT NULL, ADD month_of_birth VARCHAR(255) DEFAULT NULL, ADD year_of_birth VARCHAR(255) DEFAULT NULL, DROP city_id, DROP date_of_birth');
+        $this->addSql('ALTER TABLE stripe_account ADD city VARCHAR(255) DEFAULT NULL, ADD day_of_birth VARCHAR(255) DEFAULT NULL, ADD month_of_birth VARCHAR(255) DEFAULT NULL, ADD year_of_birth VARCHAR(255) DEFAULT NULL, DROP city_id, DROP date_of_birth');
     }
 
     /**
@@ -32,8 +29,6 @@ class Version20180119083554 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE stripe_account ADD city_id INT DEFAULT NULL, ADD date_of_birth DATE DEFAULT NULL, DROP city, DROP day_of_birth, DROP month_of_birth, DROP year_of_birth');
-        $this->addSql('ALTER TABLE stripe_account ADD CONSTRAINT FK_52F1675E8BAC62AF FOREIGN KEY (city_id) REFERENCES cities (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_52F1675E8BAC62AF ON stripe_account (city_id)');
+        $this->addSql('ALTER TABLE stripe_account ADD date_of_birth DATE DEFAULT NULL, DROP city, DROP day_of_birth, DROP month_of_birth, DROP year_of_birth');
     }
 }
