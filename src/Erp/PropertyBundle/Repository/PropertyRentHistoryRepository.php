fdfbb660fc8c8223b7c8aea4aa693eee564ea957
Dmitry Jumagaliev <d.jumagaliev@ocsico.com>
2018-01-23 16:56:25 +0300

    Added pagination to chart's tables

--- src/Erp/PropertyBundle/Repository/PropertyRentHistoryRepository.php
+++ src/Erp/PropertyBundle/Repository/PropertyRentHistoryRepository.php
@@ -8,33 +8,6 @@ use Doctrine\ORM\Query\Expr;
 
 class PropertyRentHistoryRepository extends EntityRepository
 {
-    public function getHistory1(User $user, \DateTime $dateFrom = null, \DateTime $dateTo = null, $status = null)
-    {
-        $qb = $this->createQueryBuilder('prh')
-            ->select('MAX(prh.id) as id')
-            ->join('prh.property', 'p', Expr\Join::WITH)
-            ->where('p.user = :user')
-            ->setParameter('user', $user)
-            ->groupBy('prh.property, (YEAR(prh.createdAt)), (MONTH(prh.createdAt))');
-
-        if ($dateFrom) {
-            if ($dateTo) {
-                $qb->andWhere($qb->expr()->between('prh.createdAt', ':dateFrom', ':dateTo'));
-                $qb->setParameter('dateTo', $dateTo);
-            } else {
-                $qb->andWhere('prh.createdAt > :dateFrom');
-            }
-            $qb->setParameter('dateFrom', $dateFrom);
-        }
-
-        if ($status) {
-            $qb->andWhere($qb->expr()->in('prh.status', $status));
-        }
-
-        return $qb->getQuery()
-            ->getDQL();
-    }
-
     public function getHistory(User $user, \DateTime $dateFrom = null, \DateTime $dateTo = null)
     {
         //TODO Optimize query. Find better solution
