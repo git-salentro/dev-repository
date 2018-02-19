9602bc73481529480ce7e9587adbb16c56b5e5f3
a.harbunou <Aliaksandr.Harbunou@ocsico.com>
2018-02-19 19:05:00 +0300

    ERP-65: Migration fixed manager_id

--- app/DoctrineMigrations/Version20180219141631.php
+++ app/DoctrineMigrations/Version20180219141631.php
@@ -14,6 +14,7 @@ class Version20180219141631 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('ALTER TABLE users ADD manager_id INTEGER NOT NULL');
         $this->addSql('ALTER TABLE users ADD CONSTRAINT FK_1483A5E9783E3463 FOREIGN KEY (manager_id) REFERENCES users (id);');
         $this->addSql('CREATE INDEX IDX_1483A5E9783E3463 ON users (manager_id);');
     }
