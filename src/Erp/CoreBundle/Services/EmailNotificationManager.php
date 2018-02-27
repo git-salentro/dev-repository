0bb5af8d5318da9126bd602464a2b124d01ea144
a.harbunou <Aliaksandr.Harbunou@ocsico.com>
2018-02-27 18:40:16 +0300

    ERP-111: fixtures for Email notifications (in progress)

---
+++ src/Erp/CoreBundle/Services/EmailNotificationManager.php
@@ -0,0 +1,30 @@
+<?php
+namespace ERP\CoreBundle\Services;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Erp\CoreBundle\Entity\EmailNotification;
+
+class EmailNotificationManager
+{
+    private $om;
+
+    function __construct(ObjectManager $om)
+    {
+        $this->om = $om;
+    }
+
+    public function update(EmailNotification $emailNotification, $flush = false)
+    {
+        $this->om->persist($emailNotification);
+        if ($flush) {
+            $this->om->flush();
+        }
+    }
+
+    public function delete(EmailNotification $emailNotification)
+    {
+        $this->om->remove($emailNotification);
+        $this->om->flush();
+    }
+
+}
