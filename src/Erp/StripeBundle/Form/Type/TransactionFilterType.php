af98402cf70e906c63e7d393ef54ea6c313ff432
a.harbunou <Aliaksandr.Harbunou@ocsico.com>
2018-03-01 14:03:49 +0300

    ERP-121: filter of transactions on Accounting page

--- src/Erp/StripeBundle/Form/Type/TransactionFilterType.php
+++ src/Erp/StripeBundle/Form/Type/TransactionFilterType.php
@@ -33,7 +33,7 @@ class TransactionFilterType extends AbstractFilterType
                     'label' => 'Date From',
                     'widget' => 'single_text',
                     'format' => 'MM/dd/yyyy',
-                    'attr' => ['placeholder'=>'Date From']
+                    'attr' => ['placeholder' => 'Date From', 'class' => 'form-control date']
                 ]
             )
             ->add(
@@ -43,7 +43,7 @@ class TransactionFilterType extends AbstractFilterType
                     'label' => 'Date To',
                     'widget' => 'single_text',
                     'format' => 'MM/dd/yyyy',
-                    'attr' => ['placeholder'=>'Date To']
+                    'attr' => ['placeholder' => 'Date To', 'class' => 'form-control date']
                 ]
             )
             ->add(
@@ -51,17 +51,22 @@ class TransactionFilterType extends AbstractFilterType
                 'entity',
                 [
                     'class' => User::class,
+                    'label' => 'Landlord',
+                   // 'property' => 'lastName',
+                    'required' => false,
                     'query_builder' => function (EntityRepository $repository) {
                         $user = $this->tokenStorage->getToken()->getUser();
                         $qb = $repository->createQueryBuilder('u')
                             ->where('u.manager = :manager')
                             ->setParameter('manager', $user);
-
                         return $qb;
                     },
-                    'label' => 'Landlord',
+                    'attr' => ['placeholder'=>'Landlord','class' => 'form-control']
+
                 ]
-            );
+            )
+            ->add('button', 'submit', ['label' => 'Filter', 'attr' => ['class' => 'btn red-btn', 'value' => 'Filter']]);
+        ;
     }
 
     /**
