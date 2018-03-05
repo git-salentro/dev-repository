06e034cb88d7c995c4c1daffa2306dd47ecbf35e
Dmitry Jumagaliev <d.jumagaliev@ocsico.com>
2018-03-05 19:37:45 +0300

    Separate page for accounting ledger

--- src/Erp/UserBundle/Controller/AccountingController.php
+++ src/Erp/UserBundle/Controller/AccountingController.php
@@ -24,6 +24,72 @@ class AccountingController extends BaseController
         ]);
     }
 
+    // TODO Remove that. Crate one method with showAccountingLedgerAction
+    public function listAccountingLedgerAction(Request $request, $_format = 'html')
+    {
+        /** @var TokenStorage $tokenStorage */
+        $tokenStorage = $this->get('security.token_storage');
+        /** @var User $user */
+        $user = $tokenStorage->getToken()->getUser();
+
+        $requestStack = $this->get('request_stack');
+        $masterRequest = $requestStack->getMasterRequest();
+
+        $form = $this->createForm(new TransactionFilterType($tokenStorage));
+        $form->handleRequest($masterRequest);
+
+        $data = $form->getData();
+        /** @var User $landlord */
+        $landlord = $data['landlord']; //receiver
+        $stripeAccount = $user->getStripeAccount();
+        $stripeCustomer = $landlord ? $landlord->getStripeCustomer() : null;
+        $dateFrom = $data['dateFrom'];
+        $dateTo = $data['dateTo'];
+
+        $pagination = [];
+        if ($stripeAccount) {
+            /** @var TransactionRepository $repository */
+            $repository = $this->getDoctrine()->getManagerForClass(Transaction::class)->getRepository(Transaction::class);
+            $query = $repository->getTransactionsBothDirectionsQuery($stripeAccount, $stripeCustomer, $dateFrom, $dateTo);
+
+            $paginator = $this->get('knp_paginator');
+            $pagination = $paginator->paginate(
+                $query,
+                $request->query->getInt('page', 1)
+            );
+        }
+
+        $template = sprintf('ErpUserBundle:Accounting:accounting_ledger_list.html.twig', $_format);
+        $parameters = [
+            'user' => $user,
+            'form' => $form->createView(),
+            'pagination' => $pagination,
+        ];
+
+        if ($_format == 'html') {
+            $urlParameters = array_merge(
+                ['_format' => 'pdf'],
+                ['filter' => $this->getFilterParameters($masterRequest)]
+            );
+            $parameters['pdf_link'] = $this->generateUrl('erp_user_accounting_show_accounting_ledger', $urlParameters);
+
+            return $this->render($template, $parameters);
+        } elseif ($_format == 'pdf') {
+            $fileName = sprintf('accounting_ledger_%s.pdf', (new \DateTime())->format('d_m_Y'));
+            $html = $this->renderView($template, $parameters);
+            $pdf = $this->get('knp_snappy.pdf')->getOutputFromHtml($html);
+
+            return new Response(
+                $pdf,
+                Response::HTTP_OK,
+                [
+                    'Content-Type' => 'application/pdf',
+                    'Content-Disposition'  => 'attachment; filename="' . $fileName . '"',
+                ]
+            );
+        }
+    }
+
     public function showAccountingLedgerAction(Request $request, $_format = 'html')
     {
         /** @var TokenStorage $tokenStorage */
@@ -41,6 +107,7 @@ class AccountingController extends BaseController
         /** @var User $landlord */
         $landlord = $data['landlord']; //receiver
         $stripeAccount = $user->getStripeAccount();
+        $stripeCustomer = $landlord ? $landlord->getStripeCustomer() : null;
         $dateFrom = $data['dateFrom'];
         $dateTo = $data['dateTo'];
 
@@ -48,7 +115,7 @@ class AccountingController extends BaseController
         if ($stripeAccount) {
             /** @var TransactionRepository $repository */
             $repository = $this->getDoctrine()->getManagerForClass(Transaction::class)->getRepository(Transaction::class);
-            $query = $repository->getTransactionsBothDirectionsQuery($stripeAccount, $landlord->getStripeCustomer(), $dateFrom, $dateTo);
+            $query = $repository->getTransactionsBothDirectionsQuery($stripeAccount, $stripeCustomer, $dateFrom, $dateTo);
 
             $paginator = $this->get('knp_paginator');
             $pagination = $paginator->paginate(
