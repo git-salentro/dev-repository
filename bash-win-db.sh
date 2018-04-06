2552d6896910257b65576347063663abe1b3b324
a.harbunou <Aliaksandr.Harbunou@ocsico.com>
2018-04-06 11:35:52 +0300

    Make deployment easier

--- bash-win-db.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-winpty docker-compose -f docker-compose-dev.yml exec db bash
--- bash-win-php.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-winpty docker-compose -f docker-compose-dev.yml exec php bash
