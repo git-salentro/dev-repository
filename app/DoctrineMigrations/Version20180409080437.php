6d41d643ded4ac13bd958efac1c02c56e36a5c24
a.harbunou <Aliaksandr.Harbunou@ocsico.com>
2018-04-09 11:26:57 +0300

    remove Price field from Property

--- app/DoctrineMigrations/Version20180409080437.php
+++ app/DoctrineMigrations/Version20180409080437.php
@@ -15,9 +15,6 @@ class Version20180409080437 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         $this->addSql('ALTER TABLE properties DROP price;');
-        $this->addSql('ALTER TABLE property_rent_history DROP FOREIGN KEY FK_993FB62549213EC;');
-        $this->addSql('ALTER TABLE property_rent_history ADD CONSTRAINT FK_993FB62549213EC FOREIGN KEY (property_id) REFERENCES properties (id) ON DELETE CASCADE;');
-
     }
 
     /**
