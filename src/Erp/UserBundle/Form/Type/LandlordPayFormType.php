8714e4a825f4eb0fe348e1a53ff53a30e1405edc
aman <salentrocorp@gmail.com>
2018-04-20 19:54:42 +0530

    today work in accounting ledger for pay landlord

---
+++ src/Erp/UserBundle/Form/Type/LandlordPayFormType.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Erp\UserBundle\Form\Type;
+
+use Erp\UserBundle\Entity\Charge;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class LandlordPayFormType extends AbstractType
+{
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     */
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('bank_country', 'text', ['label' => 'Country', 'required' => true, 'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank(
+                        [
+                            'message' => 'Please enter country name',
+                            'groups' => ['LandlordCharge']
+                        ]
+                    )
+                ]
+            ])
+            ->add('account_holder_name', 'text', ['label' => 'Account Holder Name', 'required' => true, 'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank(
+                        [
+                            'message' => 'Please enter bank name',
+                            'groups' => ['LandlordCharge']
+                        ]
+                    )
+                ]
+            ])
+            ->add('account_number', 'text', ['label' => 'Bank Account Number', 'required' => true, 'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank(
+                        [
+                            'message' => 'Please enter bank name',
+                            'groups' => ['LandlordCharge']
+                        ]
+                    )
+                ]
+            ])
+            ->add('amount', 'text', ['label' => 'Amount', 'required' => true, 'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank(
+                        [
+                            'message' => 'Please enter amount to charge',
+                            'groups' => ['LandlordCharge']
+                        ]
+                    ),
+                    new GreaterThan([
+                        'value' => '0.50',
+                        'groups' => ['LandlordCharge']
+                    ]),
+                    new Regex([
+                            'pattern' => '/^[0-9]{1,10}(,[0-9]{3})*(\.[0-9]+)*$/',
+                            'message' => 'Please use only positive numbers',
+                            'groups' => ['LandlordCharge']
+                        ]
+                    )
+                ]
+            ])
+            ->add('account_holder_type', 'text', ['label' => 'Account Type', 'required' => true, 'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank(
+                        [
+                            'message' => 'Please enter bank name',
+                            'groups' => ['LandlordCharge']
+                        ]
+                    )
+                ]
+            ])
+            ->add('description', 'textarea', ['label' => 'Description', 'required' => false, 'attr' => ['class' => 'form-control']])
+            ->add('button', 'submit', ['label' => 'Send charge', 'attr' => ['class' => 'btn red-btn btn-space', 'value' => 'Send charge']]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Charge::class,
+            'validation_groups' => ['LandlordCharge']
+        ]);
+    }
+
+    public function getName()
+    {
+        return 'erp_user_landlords_charge';
+    }
+}
