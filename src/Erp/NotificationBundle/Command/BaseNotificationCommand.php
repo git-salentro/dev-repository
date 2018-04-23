7f67c0a476371ac6421e8535e927f58be6bea46b
a.harbunou <Aliaksandr.Harbunou@ocsico.com>
2018-04-23 18:51:17 +0300

    Merge notifications with development

---
+++ src/Erp/NotificationBundle/Command/BaseNotificationCommand.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Erp\NotificationBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Erp\PropertyBundle\Entity\Property;
+use Erp\NotificationBundle\Entity\UserNotification;
+use Erp\NotificationBundle\Entity\History;
+use Erp\NotificationBundle\Entity\Template;
+
+class BaseNotificationCommand extends ContainerAwareCommand
+{
+    const TYPE_NOTIFICATION = 'notification';
+    const TYPE_ALERT = 'alert';
+
+    const TYPES = [
+        self::TYPE_NOTIFICATION,
+        self::TYPE_ALERT,
+    ];
+
+    protected $prefix = '';
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('erp:notification');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function process(string $type)
+    {
+        if (!in_array($type, self::TYPES)) {
+            return -1;
+        }
+
+        $userNotificationEm = $this->getEntityManager(UserNotification::class);
+        $propertyEm = $this->getEntityManager(Property::class);
+        $templateManager = $this->getContainer()->get('erp_notification.template_manager');
+        $producer = $this->getContainer()->get('old_sound_rabbit_mq.send_notification_producer');
+
+        $mailFrom = $this->getContainer()->getParameter('contact_email');
+
+        $method = null;
+        if ($type === self::TYPE_NOTIFICATION) {
+            $this->prefix = 'notification before';
+            $method = 'getPropertiesFromNotificationsIterator';
+        } elseif ($type === self::TYPE_ALERT) {
+            $this->prefix = 'alert after';
+            $method = 'getPropertiesFromAlertsIterator';
+        }
+
+        $i = 0;
+        if ($iterableResult = $userNotificationEm->getRepository(UserNotification::class)->{$method}()) {
+            foreach ($iterableResult as $propertyResult) {
+                $data = reset($propertyResult);
+                if ($property = $propertyEm->getRepository(Property::class)->find($data['propertyId'])) {
+                    $tenant = $property->getTenantUser();
+                    try {
+                        $rendered = $templateManager->renderTemplateById($data['templateId']);
+                    } catch (\Exception $ex) {
+                        $this->logRenderError($ex, $data);
+                        continue;
+                    }
+                    $msg = [
+                        'mailTo' => $tenant->getEmail(),
+                        'mailFrom' => $mailFrom,
+                        'data' => $data,
+                        'rendered' => $rendered,
+                        'tenantUser' => $tenant->getId(),
+                        'property' => $property->getId(),
+                        'prefix' => $this->prefix,
+                    ];
+                    $producer->publish(serialize($msg));
+                    $i++;
+                }
+            }
+        }
+        return $i.' tenants ('.$this->prefix.' payment date).';
+    }
+
+    private function getEntityManager($class)
+    {
+        $repository = $this->getContainer()->get('doctrine')->getManagerForClass($class);
+
+        return $repository;
+    }
+
+    private function logRenderError(\Exception $ex, $data)
+    {
+        $msg = '=============================='."\n".
+            'Render error occured for Template (trying to send '.$this->prefix.' pay date).'."\n".
+            'Data: '.var_export($data, true)."\n".
+            'Error message: '.$ex->getMessage()."\n".
+            '==============================';
+        $this->getContainer()->get('erp_notification.logger')->error($msg);
+    }
+}
