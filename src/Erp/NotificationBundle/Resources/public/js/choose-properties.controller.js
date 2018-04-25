433f3f3707434d534d5425a60041380a295dbb50
m.volkov <Maksim.Volkov@ocsico.com>
2018-04-25 10:17:18 +0300

    Fix alert/notification inputs. Disable submit buttons if no properties selected.

--- src/Erp/NotificationBundle/Resources/public/js/choose-properties.controller.js
+++ src/Erp/NotificationBundle/Resources/public/js/choose-properties.controller.js
@@ -1,9 +1,20 @@
 (function ($) {
+    var checkboxes = $('form[name="erp_notification_user_notification"] tbody input[name="idx[]"]');
+    var button = $('form[name="erp_notification_user_notification"] #erp_notification_user_notification_submit');
+
     $('form[name="erp_notification_user_notification"] input[name="all_elements"]').click(function (e) {
-        var checkboxes = $(this)
-            .closest('thead')
-            .next()
-            .find('input[name="idx[]"]')
-            .prop('checked', $(this).prop('checked'));
+            checkboxes.prop('checked', $(this).prop('checked'));
+            toggleButtonState();
     });
+
+    function toggleButtonState() {
+        if (checkboxes.filter(':checked').length) {
+            button.removeAttr('disabled');
+        } else {
+            button.attr('disabled', 'disabled');
+        }
+    }
+
+    toggleButtonState();
+    checkboxes.click(toggleButtonState);
 })(jQuery);
